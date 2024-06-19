2773e09d91933f7415d41c0a19e9edc5bde47157
Tha_UI_Guy <adeniran1059@gmail.com>
2024-06-19 17:53:18 +0200

    wip

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -76,7 +76,7 @@ onMounted(() => {
         </p>
 
         <button type="button"
-          class="rounded bg-red-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">
+          class="rounded-xl bg-red-600 px-3 py-2 text-xs font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">
           Complete
         </button>
       </div>
